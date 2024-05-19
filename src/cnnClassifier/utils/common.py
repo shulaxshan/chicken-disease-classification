c0fa6ddf417f3c0535b375ee2ac915bc0a87e58d
shulaxshan <shulazshan@gmail.com>
2024-05-19 11:52:34 +0530

    prepare base model completed

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -34,6 +34,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
     
+    
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
